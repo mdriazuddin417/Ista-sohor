548cfae995522a49dc6b575c2ba2088c020f9c11
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-03-10 22:44:43 +0600

    comment update

--- js/app.js
+++ js/app.js
@@ -57,7 +57,7 @@ const switchTab = (id) => {
 const createPost = (post) => {
   const image = post.image;
   const userImage = post.userImage;
-
+  console.log(post);
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -130,9 +130,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
